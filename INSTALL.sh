330155e7af6f5678d2f7d064a9a350c9282701cc
carlitoselmago <ccarbonell@gmail.com>
2024-05-01 12:20:49 +0200

    added force hdmi resolution and hotplug

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,6 +5,12 @@
 sudo sed -i "s/\(PrivateMounts *= *\).*/\1no/" /lib/systemd/system/systemd-udevd.service
 apt install usbmount
 
+# Enable HDMI hotplug
+sudo sed -i '/^#hdmi_force_hotplug/c\hdmi_force_hotplug=1' /boot/config.txt
+
+# Set resolution to 1920x1080
+sudo sed -i '/^#hdmi_group/c\hdmi_group=1' /boot/config.txt
+sudo sed -i '/^#hdmi_mode/c\hdmi_mode=16' /boot/config.txt
 
 #copy rc.local
 cp etc/rc.local /etc/rc.local
